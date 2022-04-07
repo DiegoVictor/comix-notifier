ce6a5b13998f81dd36548b32e6683f128bf38887
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-06 22:25:20 -0300

    Create serverless project

---
+++ serverless.ts
@@ -0,0 +1,26 @@
+import type { AWS } from "@serverless/typescript";
+
+const serverlessConfiguration: AWS = {
+  service: "comix-notifier",
+  frameworkVersion: "3",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  plugins: ["serverless-webpack"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
