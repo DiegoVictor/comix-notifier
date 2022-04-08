91a040dfa937456a84bc9f79b7b4040f7d7f336b
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-08 00:16:56 -0300

    Create function to get the one config

--- src/infra/repositories/config.ts
+++ src/infra/repositories/config.ts
@@ -1,2 +1,26 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDB, ScanCommandOutput } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+import * as Config from "@entities/Config";
+
 const dynamodb = new DynamoDB({ region: process.env.REGION });
+
+const getFirstItem = (result: ScanCommandOutput) =>
+  unmarshall(result.Items.shift());
+
+export const getOneByName = async <T>(name: string) =>
+  dynamodb
+    .scan({
+      TableName: process.env.CONFIG_TABLE,
+      ScanFilter: {
+        name: {
+          ComparisonOperator: "EQ",
+          AttributeValueList: [
+            {
+              S: name,
+            },
+          ],
+        },
+      },
+    })
+    .then((result) => Config.create<T>(getFirstItem(result)));
