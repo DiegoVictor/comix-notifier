15466aff92b50ae6dda6d5604fb6c86e5187186f
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-09 16:04:25 -0300

    Set a default value for id

--- src/entities/Config.ts
+++ src/entities/Config.ts
@@ -1,7 +1,8 @@
 import { IConfig } from "@application/contracts/IConfig";
+import { randomUUID } from "crypto";
 
 export const create = <T>({
-  id,
+  id = randomUUID(),
   name,
   value,
 }: Record<string, any>): IConfig<T> => ({
