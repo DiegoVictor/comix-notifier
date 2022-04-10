5100691f4bf6d4aef7529341725b067900188d3e
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 01:57:40 -0300

    Create product entity

---
+++ src/application/contracts/IProduct.ts
@@ -0,0 +1,5 @@
+export interface IProduct {
+  title: string;
+  number: number;
+  slug: string;
+}
---
+++ src/entities/Product.ts
@@ -0,0 +1,13 @@
+import { IProduct } from "@application/contracts/IProduct";
+
+export const PRODUCT_NAME_REGEX =
+  /<\w+\sclass="product-name"><a\shref=".+"\stitle=".+">(.+)<\/a>/gi;
+
+export const create = ({ slug, product }: Record<string, any>): IProduct => {
+  const [title, ...parts] = product
+    .replace(PRODUCT_NAME_REGEX, "$1")
+    .split(/(volume|nº)/i)
+    .map((text: string) => text.trim().replace(/,$/, ""));
+
+  return { title, number: Number(parts.pop()), slug };
+};
