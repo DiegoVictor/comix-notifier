cc2f029f3df3c81b9dc382ebcdef26dc5f39bb9a
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 01:53:49 -0300

    Set function environment variables

--- src/functions/main/index.ts
+++ src/functions/main/index.ts
@@ -2,4 +2,8 @@ import { handlerPath } from "@utils/handlerResolver";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
+  environment: {
+    CONFIG_TABLE: "${self:custom.configTable}",
+    FALLBACK_MANGA_URL: "${self:custom.fallbackMangaUrl}",
+  },
 };
