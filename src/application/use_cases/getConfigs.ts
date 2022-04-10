02deb0c2a81db415818fd9a53cb57c43d7876bcb
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 01:55:45 -0300

    Get the config existing or create

--- src/application/use_cases/getConfigs.ts
+++ src/application/use_cases/getConfigs.ts
@@ -1,8 +1,22 @@
 import * as configRepository from "@infra/repositories/config";
+import * as Config from "@entities/Config";
 import { IConfig } from "@application/contracts/IConfig";
 
 const createConfig = async <T>(config: IConfig<T>) =>
   configRepository.createOne(config);
+
+const getOrCreateConfig = async <T>(name: string, fallbackValue: T) =>
+  configRepository.getOneByName<T>(name).then(
+    (config) =>
+      config ||
+      createConfig<T>(
+        Config.create<T>({
+          name,
+          value: fallbackValue,
+        })
+      )
+  );
+
 export const getConfigs = async () =>
   Promise.all([
     configRepository.getOneByName<string[]>("urls"),
