539eaaa631f8c870c3c091211dc2468c6460fd23
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 01:59:43 -0300

    Get the last volume from page

--- src/application/use_cases/getLastVolumesFromURLs.ts
+++ src/application/use_cases/getLastVolumesFromURLs.ts
@@ -1,4 +1,6 @@
 import * as Product from "@entities/Product";
+import * as Volume from "@entities/Volume";
+import { IProduct } from "@application/contracts/IProduct";
 
 export const getSlugFromURL = (url: string) =>
   url.replace(/http:\/\/www\.comix\.com\.br\/mangas\/\w\/(.+)\.html/, "$1");
@@ -11,3 +13,12 @@ const getVolumesFromPage = ({ data: text, url }) => {
     .map((product) => Product.create({ product, slug }))
     .filter(({ number }) => !isNaN(number));
 };
+
+const getLastVolume = (products: IProduct[]) =>
+  Volume.create(
+    products.reduce(
+      (previous, product) =>
+        !previous || product.number > previous.number ? product : previous,
+      null
+    )
+  );
