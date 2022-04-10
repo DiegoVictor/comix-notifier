a870d459005454f6e33beef42fe98ecb605117b7
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 01:56:45 -0300

    Fix main function and get configs

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,5 +1,8 @@
-  try {
+import { getConfigs } from "@application/use_cases/getConfigs";
+
 export const main = async () => {
+  try {
+    const [{ value: urls }, { id, value: catalog }] = await getConfigs();
   } catch (err) {
     console.log(err);
   }
