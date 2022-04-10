d04abecd6b0484c75d2231b84743924ead5e7ada
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 02:00:12 -0300

    Create comix service

---
+++ src/infra/services/comix.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+interface Page {
+  url: string;
+  data: string;
+}
+
+const comix = axios.create();
+
+export const getPage = async (url: string) =>
+  comix.get(url).then(({ data }): Page => ({ url, data }));
