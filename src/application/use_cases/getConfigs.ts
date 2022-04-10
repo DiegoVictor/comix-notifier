1533c03d4cf75403473f901234d8dc9d516dfe44
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 01:55:05 -0300

    Create config

--- src/application/use_cases/getConfigs.ts
+++ src/application/use_cases/getConfigs.ts
@@ -1,5 +1,8 @@
 import * as configRepository from "@infra/repositories/config";
+import { IConfig } from "@application/contracts/IConfig";
 
+const createConfig = async <T>(config: IConfig<T>) =>
+  configRepository.createOne(config);
 export const getConfigs = async () =>
   Promise.all([
     configRepository.getOneByName<string[]>("urls"),
