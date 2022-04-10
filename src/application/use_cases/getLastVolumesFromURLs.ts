602f655077347a584a23ee524710800e337e2975
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-10 01:59:18 -0300

    Extract volumes from page

--- src/application/use_cases/getLastVolumesFromURLs.ts
+++ src/application/use_cases/getLastVolumesFromURLs.ts
@@ -1,2 +1,13 @@
+import * as Product from "@entities/Product";
+
 export const getSlugFromURL = (url: string) =>
   url.replace(/http:\/\/www\.comix\.com\.br\/mangas\/\w\/(.+)\.html/, "$1");
+
+const getVolumesFromPage = ({ data: text, url }) => {
+  const products: string[] = text.match(Product.PRODUCT_NAME_REGEX);
+  const slug = getSlugFromURL(url);
+
+  return products
+    .map((product) => Product.create({ product, slug }))
+    .filter(({ number }) => !isNaN(number));
+};
