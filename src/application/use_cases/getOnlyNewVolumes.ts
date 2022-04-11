4dad2fcf5d5bccea73a93dcfa2ae575d1c77b20e
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-11 16:17:03 -0300

    Compare and filter only the new volumes

---
+++ src/application/use_cases/getOnlyNewVolumes.ts
@@ -0,0 +1,9 @@
+import { IVolume } from "@application/contracts/IVolume";
+
+export const getOnlyNewVolumes = (
+  volumes: IVolume[],
+  catalog: Record<string, number>
+) =>
+  volumes.filter(
+    (volume) => !catalog[volume.slug] || volume.number > catalog[volume.slug]
+  );
