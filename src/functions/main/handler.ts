96405e1e98e8fb1815eea31cdae25bf9a54e2a7f
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-11 16:17:31 -0300

    Get new volumes

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,8 +1,15 @@
+import { getLastVolumesFromURLs } from "@application/use_cases/getLastVolumesFromURLs";
+import { getOnlyNewVolumes } from "@application/use_cases/getOnlyNewVolumes";
 import { getConfigs } from "@application/use_cases/getConfigs";
 
 export const main = async () => {
   try {
     const [{ value: urls }, { id, value: catalog }] = await getConfigs();
+
+    const volumes = await getLastVolumesFromURLs(urls).then((lastVolumes) =>
+      getOnlyNewVolumes(lastVolumes, catalog)
+    );
+
   } catch (err) {
     console.log(err);
   }
