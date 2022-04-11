4062346d0b21bbb6bf1785deac3fbe46b5db264b
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-11 16:19:17 -0300

    Create service to send notifications

---
+++ src/infra/services/notification.ts
@@ -0,0 +1,18 @@
+import { SNS } from "@aws-sdk/client-sns";
+
+const sns = new SNS({ region: process.env.REGION });
+
+export const send = async (title: string, body: string) =>
+  sns.publish({
+    Message: JSON.stringify({
+      default: "",
+      GCM: JSON.stringify({
+        notification: {
+          title,
+          body,
+        },
+      }),
+    }),
+    MessageStructure: "json",
+    TopicArn: process.env.TOPIC_ARN,
+  });
