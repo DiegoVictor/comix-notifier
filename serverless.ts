cc70acee7efd87edf304f2c78e21f7f19c786886
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-14 22:40:01 -0300

    Set region

--- serverless.ts
+++ serverless.ts
@@ -21,6 +21,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
+    region: "us-east-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
