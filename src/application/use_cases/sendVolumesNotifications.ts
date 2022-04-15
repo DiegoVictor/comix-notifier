8a15debc9e432b6b37b2a19047db7907a7ea22dd
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-15 19:52:11 -0300

    Use notification service

--- src/application/use_cases/sendVolumesNotifications.ts
+++ src/application/use_cases/sendVolumesNotifications.ts
@@ -1,9 +1,12 @@
-import { send } from "@infra/services/notification";
+import * as notificationService from "@infra/services/notification";
 import { IVolume } from "@application/contracts/IVolume";
 
 export const sendVolumesNotifications = (volumes: IVolume[]) =>
   Promise.all(
     volumes.map(({ title, number }) =>
-      send(`${title} #${number}`, `Novo volume disponível na Comix Book Shop`)
+      notificationService.send(
+        `${title} #${number}`,
+        `Novo volume disponível na Comix Book Shop`
+      )
     )
   );
