27de0c8d87ec2c47594767ffb4577818c7aff82d
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-15 17:22:05 -0300

    Create token validator

---
+++ src/application/validators/token.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const schema = z
+  .object({
+    token: z.string().min(1),
+  })
+  .required();
+
+type Params = z.infer<typeof schema>;
+
+export const token = (data: Params): Params => schema.parse(data);
