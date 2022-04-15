568182c016e7221dec57ebec66b9ffcf7efa1de9
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-15 19:52:25 -0300

    Validate token sent

--- src/functions/subscribe/handler.ts
+++ src/functions/subscribe/handler.ts
@@ -1,8 +1,15 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+import * as validate from "@application/validators/token";
+import { ZodError } from "zod";
+
 import { subscribeForNotifications } from "@application/use_cases/subscribeForNotifications";
 
 export const subscribe = async (event: APIGatewayProxyEvent) => {
   try {
+    const { token } = JSON.parse(event.body || "{}");
+
+    validate.token({ token });
+
     await subscribeForNotifications(token);
     return {
       statusCode: 204,
@@ -10,6 +17,14 @@ export const subscribe = async (event: APIGatewayProxyEvent) => {
     };
   } catch (err) {
     console.log(err);
+
+    if (err instanceof ZodError) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(err.errors),
+      };
+    }
+
     return {
       statusCode: 500,
       body: JSON.stringify({
@@ -17,4 +32,5 @@ export const subscribe = async (event: APIGatewayProxyEvent) => {
         message: "Ops! Something goes wrong, try again later.",
       }),
     };
+  }
 };
