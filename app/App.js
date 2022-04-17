a8756999a067c5c004805d46fda39aa3f161c508
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-17 00:14:08 -0300

    Subscribe device

--- app/App.js
+++ app/App.js
@@ -1,12 +1,43 @@
-import { useEffect, useState } from "react";
-import {StyleSheet, Text, View } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 import messaging from "@react-native-firebase/messaging";
 import notifee from "@notifee/react-native";
 import { StatusBar } from "expo-status-bar";
+import axios from "axios";
+import Constants from "expo-constants";
 
 export default function App() {
   const [token, setToken] = useState("");
   const [channelId, setChannelId] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [apiKey, setApiKey] = useState("");
+
+  const subscribe = useCallback(async () => {
+    try {
+      if (token.length > 0) {
+        setDisabled(true);
+
+        await axios.post(
+          Constants.manifest.extra.SUBSCRIPTION_URL,
+          {
+            token,
+          },
+          {
+            headers: {
+              "x-api-key": apiKey,
+            },
+          }
+        );
+
+        Alert.alert("Ready to receive notification!");
+      }
+    } catch (err) {
+      console.log(err);
+      Alert.alert(err.message);
+    } finally {
+      setDisabled(false);
+    }
+  }, [apiKey, token]);
 
   useEffect(() => {
     messaging()
@@ -39,6 +70,13 @@ export default function App() {
       {channelId && (
         <>
           <Text>Provide an API Key and press the send button.</Text>
+          <TextInput
+            value={apiKey}
+            onChangeText={setApiKey}
+            style={styles.input}
+            placeholder="GsvTS5VrBhKNqanESpQRTEm33SZThuj"
+          />
+          <Button title="Send" disabled={disabled} onPress={subscribe} />
         </>
       )}
     </View>
@@ -61,5 +99,16 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     paddingHorizontal: 15,
     paddingVertical: 12,
+  },
+  input: {
+    borderColor: "#eee",
+    borderRadius: 15,
+    borderWidth: 1,
+    marginBottom: 20,
+    marginTop: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    textAlign: "center",
+    width: "98%",
   },
 });
