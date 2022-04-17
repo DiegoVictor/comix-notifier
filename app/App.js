6129321f78bd099352facd13033dd8f829e47ece
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-17 00:11:18 -0300

    Show device token

--- app/App.js
+++ app/App.js
@@ -1,4 +1,46 @@
+import { useEffect, useState } from "react";
+import {StyleSheet, Text, View } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import { StatusBar } from "expo-status-bar";
+
 export default function App() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    messaging()
+      .getToken()
+      .then((deviceToken) => {
+        console.log(deviceToken);
+        setToken(deviceToken);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Retrieving the device token.</Text>
+      <Text style={styles.code}>
+        {token || "Failed to get the device token"}
+      </Text>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  code: {
+    backgroundColor: "#eee",
+    borderRadius: 15,
+    color: "#333",
+    marginVertical: 10,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+  },
 });
