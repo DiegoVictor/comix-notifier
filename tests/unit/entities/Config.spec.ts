cfe924f864ea05336d433209ffb1f114a57c6fc8
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-20 00:17:43 -0300

    Test config model

---
+++ tests/unit/entities/Config.spec.ts
@@ -0,0 +1,30 @@
+import { IConfig } from "@application/contracts/IConfig";
+import { create } from "@entities/Config";
+import faker from "@faker-js/faker";
+import factory from "../../utils/factory";
+
+const mockRandomUUID = jest.fn();
+jest.mock("crypto", () => ({
+  randomUUID: () => mockRandomUUID(),
+}));
+
+describe("Config.create", () => {
+  it("should be able to create a config instance", async () => {
+    const { id, name, value } = await factory.attrs<
+      IConfig<{ [key: string]: number }>
+    >("Config");
+
+    expect(create({ id, name, value })).toStrictEqual({ id, name, value });
+  });
+
+  it("should be able to create a config instance with id", async () => {
+    const { name, value } = await factory.attrs<
+      IConfig<{ [key: string]: number }>
+    >("Config");
+
+    const id = faker.datatype.number();
+    mockRandomUUID.mockReturnValueOnce(id);
+
+    expect(create({ name, value })).toStrictEqual({ id, name, value });
+  });
+});
