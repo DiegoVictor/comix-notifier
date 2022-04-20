12e6ae7132645e1958099bfea91c4d7ceac8f7a3
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-20 00:20:35 -0300

    Test get config use case

---
+++ tests/unit/application/getConfigs.spec.ts
@@ -0,0 +1,43 @@
+import faker from "@faker-js/faker";
+
+import { IConfig } from "@application/contracts/IConfig";
+import { getConfigs } from "@application/use_cases/getConfigs";
+import factory from "../../utils/factory";
+
+const mockGetOneByName = jest.fn();
+const mockCreateOne = jest.fn();
+jest.mock("@infra/repositories/config", () => ({
+  getOneByName: (name: string) => mockGetOneByName(name),
+  createOne: (config: IConfig<Record<string, number>>) => mockCreateOne(config),
+}));
+
+const mockCreate = jest.fn();
+jest.mock("@entities/Config", () => ({
+  create: (config: Record<string, any>) => mockCreate(config),
+}));
+
+describe("getConfigs", () => {
+  it("should be able to get existing and create non existing configs", async () => {
+    const urls = [faker.internet.url()];
+    const catalog = await factory.attrs<IConfig<Record<string, number>>>(
+      "Config"
+    );
+
+    mockGetOneByName.mockResolvedValueOnce(urls);
+    mockGetOneByName.mockResolvedValueOnce(undefined);
+    mockCreate.mockImplementation(() => catalog);
+    mockCreateOne.mockResolvedValueOnce(catalog);
+
+    const configs = await getConfigs();
+
+    expect(mockGetOneByName).toHaveBeenCalledWith("urls");
+    expect(mockGetOneByName).toHaveBeenCalledWith("catalog");
+    expect(mockCreate).toHaveBeenCalledWith({
+      name: "catalog",
+      value: {},
+    });
+    expect(configs).toContainEqual(urls);
+    expect(configs).toContainEqual(catalog);
+    expect(mockCreateOne).toHaveBeenCalledWith(catalog);
+  });
+});
