d342ea833ed2104fc6695039716754b0fb5a3c7c
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-21 00:51:14 -0300

    Test the subscription use case

---
+++ tests/unit/application/subscribeForNotifications.spec.ts
@@ -0,0 +1,18 @@
+import faker from '@faker-js/faker';
+
+import { subscribeForNotifications } from '@application/use_cases/subscribeForNotifications';
+
+const mockSubscribe = jest.fn();
+jest.mock('@infra/services/notification', () => ({
+  subscribe: (token: string) => mockSubscribe(token),
+}));
+
+describe('subscribeForNotifications', () => {
+  it('should be able to subscribe', async () => {
+    const token = faker.random.alphaNumeric(32);
+
+    await subscribeForNotifications(token);
+
+    expect(mockSubscribe).toHaveBeenCalledWith(token);
+  });
+});
