8506af9c4f6014f8eeb13b7302378114ff552e9b
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-21 00:49:53 -0300

    Test the catalog update use case

---
+++ tests/unit/application/updateCatalog.spec.ts
@@ -0,0 +1,24 @@
+import faker from '@faker-js/faker';
+
+import { IVolume } from '@application/contracts/IVolume';
+import { updateCatalog } from '@application/use_cases/updateCatalog';
+import factory from '../../utils/factory';
+
+describe('updateCatalog', () => {
+  it('should be able to update catalog', async () => {
+    const slug = faker.helpers.slugify(faker.commerce.productName());
+    const { number, title } = await factory.attrs<IVolume>('Product', { slug });
+
+    const next = number + 1;
+    expect(
+      updateCatalog(
+        {
+          [slug]: number,
+        },
+        [{ title, number: next, slug }]
+      )
+    ).toStrictEqual({
+      [slug]: next,
+    });
+  });
+});
