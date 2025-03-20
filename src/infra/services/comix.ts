ccefaad6863be9be55250781f150cd36c70db213
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-03-19 22:44:03 -0300

    Add custom User-Agent header to Axios instance for Comix service

--- src/infra/services/comix.ts
+++ src/infra/services/comix.ts
@@ -5,7 +5,12 @@ interface Page {
   data: string;
 }
 
-const comix = axios.create();
+const comix = axios.create({
+  headers: {
+    'User-Agent':
+      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0',
+  },
+});
 
 export const getPage = async (url: string) =>
   comix.get(url).then(({ data }): Page => ({ url, data }));
